fd6608f9591cf4ce4effc8438e0c1ca0e90f297c
kentaro0215 <ken.runteq0215@gmail.com>
2023-10-22 01:23:03 +0900

    テーブル変更 #12

---
+++ db/migrate/20231021161209_update_schema_for_custom_token.rb
@@ -0,0 +1,10 @@
+class UpdateSchemaForCustomToken < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :authorizations, :access_token, :string
+    remove_column :authorizations, :refresh_token, :string
+
+    add_column :users, :custom_token, :string
+
+    add_index :users, :custom_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_165714) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_161209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_165714) do
     t.bigint "user_id", null: false
     t.string "provider"
     t.string "uid"
-    t.string "access_token"
-    t.string "refresh_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_authorizations_on_user_id"
+  end
+
+  create_table "dashboards", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "start_time"
+    t.datetime "finish_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_165714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "custom_token"
+    t.index ["custom_token"], name: "index_users_on_custom_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
