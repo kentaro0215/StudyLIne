b1b43a9d042b80d110078f5cdaa07d0f2e46d124
kentaro0215 <ken.runteq0215@gmail.com>
2023-11-07 23:04:58 +0900

    Set Ruby buildpack for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_160642) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_135600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_160642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "total_time"
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
