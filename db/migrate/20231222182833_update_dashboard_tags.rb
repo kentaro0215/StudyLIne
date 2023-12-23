543aa63136bed82565a7e19051b2e351f3304cbe
kentaro0215 <ken.runteq0215@gmail.com>
2023-12-23 18:23:09 +0900

    テーブル名 #27

---
+++ db/migrate/20231222182833_update_dashboard_tags.rb
@@ -0,0 +1,7 @@
+class UpdateDashboardTags < ActiveRecord::Migration[7.0]
+  def change
+    rename_table :dashboard_tags, :study_record_tags
+
+    rename_column :study_record_tags, :dashboard_id, :study_record_id
+  end
+end
